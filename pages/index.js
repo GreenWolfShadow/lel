932d2e4d69fdd296e058202ef539ea7c4db5f37c
bfuerr <bfuerr@bbcag.ch>
2022-06-09 10:46:06 +0200

    hahah

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import {useEffect, useState} from "react";
 
 export default function Home () {
@@ -11,62 +8,54 @@ export default function Home () {
         gamma: 0
     })
 
-    function handlePermission() {
-        navigator.permissions.query({name:'accelerometer'}).then(function(result) {
-            if (result.state == 'granted') {
-                report(result.state);
-            } else if (result.state == 'prompt') {
-                report(result.state);
-            } else if (result.state == 'denied') {
-                report(result.state);
-            }
-            result.addEventListener('change', function() {
-                report(result.state);
-            });
-        });
-    }
 
     function report(state) {
         console.log('Permission ' + state);
     }
 
     useEffect(() => {
-        if (window === "undefined") return;
-        console.log(window)
+        function handlePermission() {
+            navigator.permissions.query({name:'accelerometer'}).then(res => {
+                if (res.state == 'granted') {
+                    report(res.state);
+                } else if (res.state == 'prompt') {
+                    report(res.state);
+                } else if (res.state == 'denied') {
+                    report(res.state);
+                }
+                res.addEventListener('change', function() {
+                    report(res.state);
+                });
+            });
+        }
+
+        function handleOrientation(event) {
+            const alpha = event.alpha;
+            const beta = event.beta;
+            const gamma = event.gamma;
+            console.log(pos);
+            setSomeText(JSON.stringify(event, 2, 2));
+            console.log(alpha, beta, gamma);
+            if(pos === undefined) {
+                console.log(pos)
+                setPos({
+                    alpha,
+                    beta,
+                    gamma
+                })
+                return
+            }
+            if(Math.abs(alpha - pos.alpha) > 10){
+                console.log("yes");
+            }
+        }
+
+        if (!window) return;
         handlePermission();
         // window.addEventListener('accelerometer', handleOrientation);
         window.addEventListener('devicemotion', handleOrientation);
-    }, [handleOrientation, handlePermission])
+    }, [pos])
 
-
-
-
-    function handleOrientation(event) {
-        const alpha = event.alpha;
-        const beta = event.beta;
-        const gamma = event.gamma;
-        console.log(pos)
-        setSomeText(JSON.stringify(event, 2, 2));
-        if(pos === undefined) {
-            console.log(pos)
-            this.setPos({
-                alpha,
-                beta,
-                gamma
-            })
-            return
-        }
-        setPos({
-            alpha: event.alpha,
-            beta: event.beta,
-            gamma: event.gamma
-        })
-        if(Math.abs(alpha - pos.alpha) > 10){
-            console.log("yes");
-        }
-
-
-    }
 
   return (
       <pre>{someText}</pre>
